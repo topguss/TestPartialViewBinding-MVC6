1b6edb5b9e8dff57abcf69471416d7fc022295b5
topguss <igor.bojanic@lanaco.com>
2016-02-18 08:43:41 +0100

    Test project for Non-deterministic behavior for model in PartialView
    This is test project for reported issui #4106 'Non-deterministic
    behavior for model in PartialView' on Asp.Net/MVC

---
+++ src/TestPartialViewBinding/Controllers/PaymentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestPartialViewBinding.ViewModel;
+
+namespace TestPartialViewBinding.Controllers
+{
+  public class PaymentController : Controller
+  {
+
+    public IActionResult CreatePayment()
+    {
+      PaymentViewModel payment = new PaymentViewModel()
+      {
+        Payment = 321,
+        Description = "Regular 321"
+      };
+
+      payment.SplittedPayment.Add(new SplittedPaymentViewModel()
+      {
+        SplittedPaymentId = 0,
+        Payment = 123,
+        Description = "Partial 123"
+      });
+
+      payment.SplittedPayment.Add(new SplittedPaymentViewModel()
+      {
+        SplittedPaymentId = 1,
+        Payment = 456,
+        Description = "Partial 456"
+      });
+
+      return View("CreatePayment", payment);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult CreatePayment(PaymentViewModel model)
+    {
+      return View("CreatePayment", model);
+    }
+
+    public IActionResult SplittedPaymentPartial()
+    {
+      SplittedPaymentViewModel splittedPayment = new SplittedPaymentViewModel()
+      {
+        SplittedPaymentId = 1,
+        Payment = 123,
+        Description = "Partial 123"
+      };
+
+      return PartialView("SplittedPaymentPartial", splittedPayment);
+    }
+  }
+}
---
+++ src/TestPartialViewBinding/ViewModel/PaymentViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestPartialViewBinding.ViewModel
+{
+  public class PaymentViewModel
+  {
+    public PaymentViewModel()
+    {
+      SplittedPayment = new List<SplittedPaymentViewModel>();
+    }
+
+    public decimal? Payment { get; set; }
+
+    [StringLength(10, MinimumLength = 1)]
+    public string Description { get; set; }
+
+    public List<SplittedPaymentViewModel> SplittedPayment { get; set; }
+  }
+
+  public class SplittedPaymentViewModel
+  {
+    public short SplittedPaymentId { get; set; }
+
+    public decimal? Payment { get; set; }
+
+    [StringLength(20, MinimumLength = 2)]
+    public string Description { get; set; }
+  }
+}
